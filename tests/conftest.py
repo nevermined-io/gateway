d3199e7738ea25237a62b3adb9967929910f1c6a
eruizgar91 <enrique@keyko.io>
2020-04-30 09:23:46 +0200

    Fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,6 +47,6 @@ def get_consumer_account():
 
 def get_sample_ddo():
     return json.loads(urlopen(
-        "https://raw.githubusercontent.com/keyko-io/nevermined-docs/master/architecture/specs"
-        "/examples/access/v0.1/ddo1.json").read().decode(
+        'https://raw.githubusercontent.com/keyko-io/nevermined-docs/master/architecture/specs'
+        '/examples/access/v0.1/ddo1.json').read().decode(
         'utf-8'))
