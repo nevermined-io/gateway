0a3abe511f52c99332d5a321ab9f790c78968291
Aitor <1726644+aaitor@users.noreply.github.com>
2020-05-11 16:24:14 +0200

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/nevermined-gateway',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
 )
