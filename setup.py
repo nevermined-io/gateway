f3d936fb3c432b496261bfe3224a866c1421d65b
Aitor <1726644+aaitor@users.noreply.github.com>
2020-05-12 14:10:48 +0200

    updating to last commons-utils version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('CHANGELOG.md') as history_file:
 # or pip install -e .
 install_requirements = [
     # Install squid-py and all its dependencies first
-    'common-utils-py==0.2.3',
+    'common-utils-py==0.2.4',
     'contracts-lib-py==0.2.2',
     'ocean-secret-store-client==0.0.2',
     'Flask==1.1.2',
