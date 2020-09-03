ef73157a8a380479f8f6d8f6ed76b265650f8e67
Aitor <1726644+aaitor@users.noreply.github.com>
2020-09-03 16:38:27 +0200

    v0.4.1

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/nevermined-gateway',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
 )
