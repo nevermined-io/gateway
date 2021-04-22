c8c2dc8333ea4ef3e662eedf1a06c51e6e3d9dac
Aitor <1726644+aaitor@users.noreply.github.com>
2021-04-22 10:19:43 +0200

    If the token address is empty we assume Nevermined token address

--- nevermined_gateway/conditions.py
+++ nevermined_gateway/conditions.py
@@ -107,6 +107,8 @@ def fulfill_escrow_payment_condition(keeper, agreement_id, cond_ids, asset, prov
         amounts = list(map(int, service_agreement.get_param_value_by_name('_amounts')))
         receivers = to_checksum_addresses(service_agreement.get_param_value_by_name('_receivers'))
         token_address = service_agreement.get_param_value_by_name('_tokenAddress')
+        if token_address is None or len(token_address) == 0:
+            token_address = keeper.token.address
 
         recheck_condition = False
         try:
