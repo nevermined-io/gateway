4688f990333d3c1501fa90ca753be5f1cc0b4e72
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-04-22 15:11:03 +0200

    Bump version to v0.8.1

--- setup.py
+++ setup.py
@@ -89,6 +89,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/gateway',
-    version='0.8.0',
+    version='0.8.1',
     zip_safe=False,
 )
