2cb0fc903bac4e3b99c32e285b71c7af21ff0ff6
Aitor <1726644+aaitor@users.noreply.github.com>
2021-05-20 19:09:14 +0200

    Adapting to last version of contracts with new DID registration procedure

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ with open("nevermined_gateway/version.py") as version_file:
 # or pip install -e .
 install_requirements = [
     # Install squid-py and all its dependencies first
-    'common-utils-py==0.7.0',
-    'contracts-lib-py==0.7.2',
+    'common-utils-py==0.7.1',
+    'contracts-lib-py==0.7.4',
     'nevermined-secret-store==0.1.1',
     'Flask==1.1.2',
     'Flask-Cors==3.0.8',
