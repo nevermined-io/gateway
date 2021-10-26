017f885b46476f2652492b57c91ffd52e45024c9
Sami Mäkelä <mrsmkl@gmail.com>
2021-10-26 13:27:15 +0300

    make rapidsnark optional

--- nevermined_gateway/snark_util.py
+++ nevermined_gateway/snark_util.py
@@ -2,11 +2,16 @@ from common_utils_py.utils.keytransfer import make_prover, prove_transfer, hash_
 from nevermined_gateway.util import get_config
 from web3 import Web3
 
-config = get_config()
+prover = 0
 
-prover = make_prover(config.keytransfer_zkey, config.keytransfer_dat)
+def init_prover():
+    global prover
+    config = get_config()
+    prover = make_prover(config.keytransfer_zkey, config.keytransfer_dat)
 
 def call_prover(consumer_pub, provider_secret, asset_plain):
+    if prover == 0:
+        init_prover()
     c = Web3.keccak(text=provider_secret)
     provider_key=c.hex()[0:60]
     res = prove_transfer(prover, [int(consumer_pub[0], 16), int(consumer_pub[1], 16)], int(provider_key, 16), bytes.fromhex(asset_plain[2:]))
